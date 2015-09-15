2dfd0cfe69d6d96074951da8da082830712eb52f
Diego Rueda <diego.rueda2@gmail.com>
2015-09-15 15:16:47 -0400

    Snackbar

--- app/src/main/java/com/dev/diegorueda/desingdemo/MainActivity.java
+++ app/src/main/java/com/dev/diegorueda/desingdemo/MainActivity.java
@@ -1,5 +1,7 @@
 package com.dev.diegorueda.desingdemo;
 
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
@@ -8,6 +10,8 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +30,19 @@ public class MainActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
         actionBar.setDisplayHomeAsUpEnabled(true);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(findViewById(R.id.drawer_layout), "Soy un Snackbar", Snackbar.LENGTH_LONG).setAction("Action", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(MainActivity.this, "Snackbar Action link", Toast.LENGTH_LONG).show();
+                    }
+                }).show();
+            }
+        });
     }
 
     @Override
